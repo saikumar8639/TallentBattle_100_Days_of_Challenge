0b9215f2285cf7a8139fdd3f148d0fa717043bda
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-17 22:04:57 +0530

    first commit

---
+++ day2.py
@@ -0,0 +1,14 @@
+"""
+Day 2 coding Statement: Write a program to identify if the character is an alphabet or not.
+
+Description: Take an input character from user and check whether it is an alphabet or not.
+
+ 
+Input : A
+Output:  Alphabet
+
+Input: 7
+Output: Not an alphabet
+
+"""
+s=input()

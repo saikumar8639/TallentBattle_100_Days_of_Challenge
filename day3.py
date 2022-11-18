7e83363f2df04db28a1e4b74f499b7891c73fcb3
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-18 21:04:00 +0530

    first commit

---
+++ day3.py
@@ -0,0 +1,17 @@
+"""
+Day 3 coding Statement: Write a program to find ASCII values of a character
+
+Description: Get an input character from the user and give the ASCII value of the given input as the output.
+
+Input: b
+
+Output: 98
+
+Input: B
+
+Output: 66
+
+"""
+
+s=input()
+print(ord(s))

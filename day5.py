992f62727e891c78dc27c3d9b53f188610b28a2e
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-20 17:59:19 +0530

    first commit

---
+++ day5.py
@@ -0,0 +1,20 @@
+"""
+
+Day 5 coding Statement:  Write a program to identify if the number is even or odd
+Description
+Get a number as input from the user and check whether the given number is odd or even
+
+Input
+10
+
+Output
+Even
+
+Input
+5
+
+Output
+Odd
+
+"""
+number=int(input())

ba5b1194b97d6b9e9d3c515686993ace750e965b
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-21 17:38:48 +0530

    first commit

---
+++ day6.py
@@ -0,0 +1,17 @@
+"""
+Day 6 coding Statement:  Write a program to find the Quadrants in which coordinates lie
+
+Get the value of x and y coordinates as input from the user and check in which quadrant the point lies and print it.
+
+Input
+10 20
+
+Output
+This point lies in first quadrant.
+
+Input
+-10 20
+
+Output
+This point lies in second quadrant.
+"""

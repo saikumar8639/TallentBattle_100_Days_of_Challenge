196f3e638f4b9307ab2cf8551716f83d92fc268b
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-23 17:15:33 +0530

    first commit

---
+++ day8.py
@@ -0,0 +1,14 @@
+"""
+Day 8 coding Statement:  Write a program to find roots of a quadratic equation
+
+Description
+
+Get the values of a, b and c (coefficients of quadratic equation) as input from the user and calculate the roots and print as the output.
+
+Input
+Enter the value of a, b and c : 1 -6 9
+
+Output
+Roots are equal
+Root 1= root 2 = 3.00
+"""

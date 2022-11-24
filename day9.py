2bf44f728f1f6c3dd577040e15585b9da2ed8689
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-24 18:07:58 +0530

    updating the question

---
+++ day9.py
@@ -0,0 +1,20 @@
+"""
+
+Day 9 coding Statement : Write a program to find Number of digits in an integer
+
+Description
+
+Take an integer as the input from the user and then calculate the number of digits in the given input and print it as the output.
+
+Input
+3241
+
+Output
+4 
+
+Input
+6
+
+Output
+1
+"""

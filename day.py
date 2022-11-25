daa36c29ff340254ef690f55814527ef66c41b5f
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-25 22:27:59 +0530

    question

---
+++ day.py
@@ -0,0 +1,13 @@
+"""
+
+Day 10 coding Statement:  Write a program to find Factorial of a number
+Description
+Get a number from user for which you need to fin the factorial, then calculate the factorial and give it as the output.
+
+Input
+4
+
+Output
+24
+"""
+

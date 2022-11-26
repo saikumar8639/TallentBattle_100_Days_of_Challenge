2e594789550b056bc6c43c9c7638cde3d6f290c0
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-26 19:58:06 +0530

    question

---
+++ day11.py
@@ -0,0 +1,19 @@
+"""
+Day 11 coding Statement:  Write a program to find Fibonacci series up to n
+
+Description
+
+Fibonacci series is a special series where nth term is the sum of previous two terms in the series. The series starts with 0 and 1 as the first and second term of the series respectively.
+
+Here you need to get the value for nth term from user and then print Fibonacci series containing n terms.
+
+Input
+5
+Output
+0,1,1,2,3
+
+Input
+8
+Output
+0,1,1,2,3,5,8,13
+"""

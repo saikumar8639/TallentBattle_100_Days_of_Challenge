f275183052cf264f30698fd067b910d1a5058d51
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-27 21:29:42 +0530

    question

---
+++ day12.py
@@ -0,0 +1,16 @@
+"""
+Day 12 coding Statement:  Write a program to find Sum of digits of a number
+
+Description
+
+Get a number from user and then find the sum of the digits in the given number.
+
+E.g. let the number = 341
+Sum of digits is 3+4+1= 8
+
+Input
+4521
+
+Output
+12
+"""

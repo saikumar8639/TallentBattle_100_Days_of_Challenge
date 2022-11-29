18c0c1c00f313d311e0e9fb15343ed3d862a566e
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-29 17:22:23 +0530

    question

---
+++ day14.py
@@ -0,0 +1,15 @@
+"""
+Day 14 coding Statement : Write a program to reverse a given number
+
+Description
+
+Get an input from the user and the print the reverse of the given number as the output
+
+E.g. let the number be 324 then the reverse of the number is 423
+
+Input
+675
+
+Output
+576
+"""

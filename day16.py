bb2e9b7067d4e5d3941af577990d0f1807b076f1
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-01 19:11:02 +0530

    question

---
+++ day16.py
@@ -0,0 +1,21 @@
+"""
+Day 16 coding Statement : Write a program to identify if the number is Perfect number or not
+
+Description
+
+Get the input from the user and check whether that number is a perfect number or not.
+
+E.g. Let number is 28, factors of 28 are 1,2,4,7,14. Now the sum of all these factors are 28 itself.
+
+Input
+28
+
+Output
+Perfect Number
+
+Input
+4
+
+Output
+Not a perfect number 
+"""

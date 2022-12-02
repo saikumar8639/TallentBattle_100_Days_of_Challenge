5d436cf8aac86ebe532ec474691740e1d6b9041e
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-02 17:49:48 +0530

    question

---
+++ day17.py
@@ -0,0 +1,13 @@
+""""""""
+Day 17 coding Statement : Write a program to find the Factors of a number
+
+Description
+Get an input from the user and find the factors of the number.
+
+Input
+4
+
+Output
+1,2,4  
+"""
+

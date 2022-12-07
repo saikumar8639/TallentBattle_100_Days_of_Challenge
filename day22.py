385e557dd85770c9081025e4399f35b9f5b6f55c
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-07 23:24:33 +0530

    final solution

--- day22.py
+++ day22.py
@@ -12,4 +12,16 @@ Output
 4 can be expressed as sum of 2 and 2
 
 """
-
+def isprime(n):
+    for i in range(2,int(n**0.5)):
+        if n%i==0:
+            return False
+    return True
+num=int(input())
+i=2
+n=num//2 if num%2==0 else num//2+1
+while (i<=n):
+    if isprime(i) and isprime(num-i):
+        print(num,"can be expressed as sum of",i,"and",num-i)
+        break
+    i+=1

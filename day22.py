c416bc7baa42374a05a69b62a36c1be2939ad1f4
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-07 23:16:32 +0530

    question

---
+++ day22.py
@@ -0,0 +1,15 @@
+"""
+Day 22 coding Statement : Write a program to express a number as a sum of two prime numbers
+
+Description
+
+Get a number as input from the user and express that number as sum of two prime numbers.
+
+Input
+4
+
+Output
+4 can be expressed as sum of 2 and 2
+
+"""
+

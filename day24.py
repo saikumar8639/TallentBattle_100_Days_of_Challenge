3ebf516a4ccca2c38734a47525798f832ab6ac4f
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-09 21:41:36 +0530

    question

---
+++ day24.py
@@ -0,0 +1,21 @@
+"""
+Day 24 coding Statement : Write a program to print Pyramid pattern using stars
+
+Description
+
+Get the number of lines as the input and print stars in that many lines or rows in a pyramid shape.
+
+Input
+
+4
+
+Output
+
+  *
+
+ ***
+
+*****
+
+*******
+"""

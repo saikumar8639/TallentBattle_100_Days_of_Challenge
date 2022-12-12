8f7937cd1d9e9d41a81c26e66a61178b53cbf9f8
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-12 23:13:05 +0530

    first commit

---
+++ day27.py
@@ -0,0 +1,13 @@
+"""
+Day 27 coding Statement : Write a program to find the double of the given number without using arithmetic operator
+
+Description
+
+For the given input number calculate the double of it without using arithmetic operator.
+
+Input
+4
+
+Output
+8
+"""

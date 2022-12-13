c2f3d7b32f21c9b43db98495a989868d8fa66c8d
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-13 22:54:18 +0530

    question

---
+++ day28.py
@@ -0,0 +1,15 @@
+"""
+Day 28 coding Statement : Write a Program to reverse a string.
+
+Description
+
+Get an input string from user and print it in reverse order.
+
+Input
+
+Hello
+
+Output
+
+olleH
+"""

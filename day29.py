475c8f64786c0d9959c76a1b537e842db1d2ce3d
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-15 15:03:48 +0530

    final solution

--- day29.py
+++ day29.py
@@ -1,5 +1,23 @@
+"""
+Day 29 coding Statement : Write a Program to concatenate a string.
 
+Description
+
+Get two strings as input from the user and then concatenate it.
+
+Input
+
+Enter first string
+Hello
+
+Enter second string
+Hi
+
+Output
+HelloHi
+"""
 x = input("Enter first string")
 
 y = input("Enter second string")
 print("".join([x, y]))
+

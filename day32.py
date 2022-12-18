7ac2aace8861705d3c4252f8576e2ba8855d55a7
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-18 10:29:29 +0530

    question

--- day32.py
+++ day32.py
@@ -1,3 +1,15 @@
 """
-question
+Day 32 coding Statement : Write a Program to Remove vowels from a string
+
+Description
+
+Get a string as the input from the user and then remove all the vowel letters from the string and give the output.
+
+Input
+
+remove
+
+Output
+
+rmv
 """

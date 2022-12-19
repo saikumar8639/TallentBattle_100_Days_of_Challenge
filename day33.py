4a7c8fbc1f1b0e19329df4982d4dd78be8d30d85
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-19 08:43:53 +0530

    question

---
+++ day33.py
@@ -0,0 +1,19 @@
+"""
+Day 33 coding Statement : Write a Program to check if String is a palindrome or not
+
+Description
+
+Get an input string from the user and then check whether it is a palindrome string or not.
+
+Input
+noon
+
+Output
+Palindrome
+
+Input
+Talent
+
+Output
+Not a Palindrome
+"""

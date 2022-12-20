728ed2bbd9eb00d5c952a8395e08b8785a5c4013
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-20 21:00:01 +0530

    question

---
+++ day35.py
@@ -0,0 +1,16 @@
+"""
+Day 35 coding Statement : Write a Program to Count the sum of numbers in a string
+
+Description
+
+Get a string from the user and find the sum of numbers in the string.
+
+Input
+
+Hello56
+
+Output
+
+11
+"""
+#Python solution

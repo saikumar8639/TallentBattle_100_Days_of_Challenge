c682cdcbf409b4575eb3fc4ebbaf1da6278e7588
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-12-23 20:50:56 +0530

    first commit

---
+++ day38.py
@@ -0,0 +1,28 @@
+"""
+Day 38 coding Statement : Write a Program to print Non-repeating characters in a string
+
+Description
+
+Get a string as the input from the user and print the non-repeating characters in a string.
+
+Input
+
+Hello
+
+Output
+
+H e o
+
+ 
+
+To solve the above coding statement, if you need, you can refer to below learning modules.
+
+C Module to Refer: Flow control, Functions, Operators, String
+
+Python Module to refer: Flow control, Functions, Operators, String
+
+C++ Module to Refer: Flow control, Functions, Operators, String
+
+Java Modules to Refer: Flow control, Functions, Operators, String
+
+"""

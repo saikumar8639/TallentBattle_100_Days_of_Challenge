047a703e328841e690e91567d4059b7d19f60a99
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-01-01 19:01:37 +0530

    final commit

--- day46.py
+++ day46.py
@@ -1,4 +1,17 @@
 """
-Day 46 """
+Day 46 coding Statement : Write Program to find sum of elements in an array
+Description
+Get an array as the input from the user and find the sum of the elements.
+Input
+
+Enter the size of array
+3
+
+Enter the elements of array
+5 10 15
+
+Output
+30
+"""
 #Python solution
 #solution

76d2829408d4918325fa185446b91a2377554554
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-01-08 21:42:30 +0530

    first commit

---
+++ day53.py
@@ -0,0 +1,14 @@
+"""
+Day 53 coding Statement : Given an integer array of size N. Write Program to find maximum product subarray in a given array.
+Sample input 1:
+4
+2 -4 -1 -3
+Sample output 1:
+8 = {2, -4, -1}
+
+Sample input 2:
+5
+1 5 -7 5 3
+Sample output 2:
+15 = {5, 3}
+"""

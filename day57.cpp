07ad2281037cf0925d0fd39a8c9a9fdf23db9d29
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-01-12 20:09:23 +0530

    final commit

--- day57.cpp
+++ day57.cpp
@@ -5,10 +5,21 @@ This program stores the information (name, roll and marks entered by the user) o
 in a structure and displays it on the screen.
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    cout<<"sai";   
-    return 0;
+struct student {
+   int rollNo;
+   char name[50];
+   float marks;
+   char grade;
+};
+int main() {
+   struct student s = { 12 , "Harry" , 90 , 'A' };
+   cout<<"The student information is given as follows:"<<endl;
+   cout<<endl;
+   cout<<"Roll Number: "<<s.rollNo<<endl;
+   cout<<"Name: "<<s.name<<endl;
+   cout<<"Marks: "<<s.marks<<endl;
+   cout<<"Grade: "<<s.grade<<endl;
+   return 0;
 }

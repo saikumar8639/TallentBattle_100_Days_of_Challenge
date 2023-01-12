ac753eab25f3bc1310750e3b88fc5556e84e784d
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-01-12 19:58:15 +0530

    final commit

---
+++ day57.cpp
@@ -0,0 +1,14 @@
+/*
+Day 57 coding Statement : C++ Program to Store Information of a Student in a Structure
+
+This program stores the information (name, roll and marks entered by the user) of a student 
+in a structure and displays it on the screen.
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"sai";   
+    return 0;
+}

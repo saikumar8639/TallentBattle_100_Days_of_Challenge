f8a023ed5b9e2a7f7509fd726a654a07d7ba7f42
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-01-16 21:25:23 +0530

    final commit

--- day61.py
+++ day61.py
@@ -27,3 +27,14 @@ Sample Output 1
 4
 3
 """
+#Python solution
+for _ in range(int(input())):
+    a,b=map(int,input().split())
+    if a+b<3:
+        print(1)
+    elif a+b<=10:
+        print(2)
+    elif a+b<=60:
+        print(3)
+    else:
+        print(4)

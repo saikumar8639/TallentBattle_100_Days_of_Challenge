3c9158b21eb67ce50b9ef6598fdb25b466d06260
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-02-01 08:34:49 +0530

    final commit

--- day74.py
+++ day74.py
@@ -32,3 +32,13 @@ Sample Output
 0
 2
 """
+for _ in range(intinput()):
+    m,n=map(int,input().split())
+    if m%2==0 and n%2==0:
+        print("0")
+    elif m%2==0 and n%2==1:
+        print(m)
+    elif m%2==1 and n%2==0:
+        print(n)
+    else:
+        print(m+n-1)

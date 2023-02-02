8432a3acb56e66db4a050baf0c3fc883b2a73131
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-02-02 22:34:53 +0530

    final commit

--- day76.py
+++ day76.py
@@ -35,3 +35,25 @@ Sample Output
 1
 
 2"""
+def check(x):
+    ls = [[] for _ in range(x)]; cIND = 0
+    for i in range(n):
+        if len(ls[cIND])==k:
+            return True
+    if len(ls[cIND])==0 or ls[cIND][-1]*c<=a[i]:
+        ls[cIND].append(a[i])
+    cIND = (cIND+1)%x
+    if len(ls[cIND])==k:
+        return True
+    return False
+for _ in range(int(input())):
+    n,k,c = map(int,input().split())
+    a = list(map(int,input().split()))
+    a.sort(); l = 0; r = n+1
+    while r-1>l:
+        mid = (l+r)//2
+        if check(mid):
+            l = mid
+        else:
+            r = mid
+    print(l)

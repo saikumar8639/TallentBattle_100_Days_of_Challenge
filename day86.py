f247f7342616881ca015fa95c326b5361b5aba2d
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-02-12 21:07:30 +0530

    final solution

--- day86.py
+++ day86.py
@@ -31,6 +31,33 @@ Sample Output
 10
 1 1
 """
+def binarySearch (arr, l, r, x):
+    if r >= l:
+        mid = l + (r - l) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binarySearch(arr, mid+1, r, x)
+        else:
+            return binarySearch(arr, l, mid-1, x)
+    else:
+        return -1
 for _ in range(int(input())):
     n=int(input())
-    arr=[int(i) for i in input().split()]
+    arr=sorted([int(i) for i in input().split()],reverse=True)
+    arr.pop()
+    original_set = []
+    to_be_removed = []
+
+    while len(original_set) < n:
+        element = arr.pop()
+        original_set.append(element)
+        will_be_removed = [element]
+        for rem_val in to_be_removed:
+            new_rem_val = rem_val + element
+            will_be_removed.append(new_rem_val)
+            idx = binarySearch(arr, 0, len(arr) - 1, new_rem_val)
+            arr.pop(idx)
+        to_be_removed += will_be_removed
+
+    print(*sorted(original_set),sep = " ")

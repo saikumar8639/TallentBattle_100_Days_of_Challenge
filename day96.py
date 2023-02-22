23c1fa411ee837c12380a22a1a0c967514a67aac
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-02-22 22:28:24 +0530

    final solution

--- day96.py
+++ day96.py
@@ -37,3 +37,15 @@ Sample Output
 BWBW
 
 """
+for _ in range(int(input())):
+    x=input()
+    y=input()
+    s=''
+    for i in range(len(x)):
+        if x[i]==y[i] and x[i]=='B':
+            s+='W'
+        elif x[i]==y[i] and x[i]=='W':
+            s+='B'
+        else:
+            s+='B'
+    print(s)

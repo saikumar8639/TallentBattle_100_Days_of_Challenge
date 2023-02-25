542e5a06618af612c4d331844b08cad41e6bbc51
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-02-25 22:32:33 +0530

    final commit

--- day99.py
+++ day99.py
@@ -47,3 +47,27 @@ Sample Output
 
 5
 """
+for _ in range(int(input())):
+    n,m = map(int,input().split())
+    v = []
+    for _m in range(m):
+        x,y = map(int,input().split())
+        v.append([y,x])
+    v.sort()
+    l = 0
+    r = n - 1
+    cnt = -1
+    while(l <= r):
+        mid = (l+r)//2
+        seg = 1
+        for i in range(m):
+            if(seg >= v[i][0] or v[i][1] > seg):
+                continue
+            else:
+                seg = min(seg + mid,v[i][0])
+        if(seg == n):
+            cnt = mid
+            r = mid - 1
+        else:
+            l = mid + 1
+    print(cnt)

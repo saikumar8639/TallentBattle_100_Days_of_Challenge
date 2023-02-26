9b0da3ab66ab8c1a40e6660a24aa51f9132b589a
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2023-02-26 19:37:14 +0530

    python solution

--- day100.py
+++ day100.py
@@ -34,3 +34,19 @@ Sample Output
 0
 
  """
+from collections import Counter
+for _ in range(int(input())):
+    a=list(map(int,input().split()))
+    b=Counter(a)
+    a=[]
+    for k,v in b.items():
+        a.append(v)
+
+    if 4 in a:
+        print("0")
+    elif 3 in a:
+        print("1")
+    p=a.count(2)
+    q=a.count(1)
+    if p==2 or p==1 or q==4:
+        print("2")
